ad1ba1782ed02883491cc3c9b70c6875b5d1bd6b
翁旻醇 <d0.0crazyd@gmail.com>
2024-07-22 16:48:29 +0800

    ImgWithLoading

--- src/pages/components/LoadingAndImg.jsx
+++ src/pages/components/LoadingAndImg.jsx
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 const ImgWithLoading = ({ src, alt, className }) => {
+  const [imgSrc, setImgSrc] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  const handleImageLoad = () => {
-    console.log('Image loaded');
-    setIsLoading(false);
-  };
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      setImgSrc(src);
+      setIsLoading(false);
+    };
+    img.onerror = () => {
+      console.error('Image failed to load');
+      setIsLoading(false);
+    };
+  }, [src]);
 
   return (
-    <div >
-      {isLoading && (
+    <div className={className}>
+      {isLoading ? (
         <svg xmlns="http://www.w3.org/2000/svg" className={`${className}`}>
           <rect width="100%" height="100%" fill="#2F384E" />
         </svg>
-
+      ) : (
+        <img
+          src={imgSrc}
+          alt={alt}
+          className={`w-full h-full object-cover ${className}`}
+        />
       )}
-      <img
-        src={src}
-        alt={alt}
-        className={`${className}`}
-        onLoad={handleImageLoad}
-      />
     </div>
   );
 };
